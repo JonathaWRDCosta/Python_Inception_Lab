7d94221ddd7bbc54378332548a6c48ed1a632193
Jonatha Costa <jonathawiliamsrdcosta@gmail.com>
2023-12-19 11:25:35 +0000

    P_01

---
+++ loop_for.py
@@ -0,0 +1,33 @@
+"""
+Loop for
+
+Loop -> Estrutura de repetição.
+For -> É uma dessas estruturas.
+
+C ou Java
+
+for (int i = 0; i < (Um limitador); i++)
+{
+  //Execução do loop
+}
+
+Python
+
+for item in interável:
+  //Execução do loop
+
+Utilizamos loops para iterar sobre sequências ou sobre valores interáveis
+
+Exemplos de intereáveis:
+
+- String
+  nome = 'Geek University'
+- Lista
+  lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+- Range
+  numeros = range(1, 10)
+"""
+
+nome = 'Geek Univeristy'
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+numeros = range(1, 10)
